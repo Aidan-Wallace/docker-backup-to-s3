90cccb32d43d4fd20ebf72927708477e96b698f8
Aidan <77929240+Aidan-Wallace@users.noreply.github.com>
2024-05-01 22:55:41 -0400

    do not decrypt and get most recent object

--- app/restore.sh
+++ app/restore.sh
@@ -2,14 +2,14 @@
 
 test -z $DEBUG || set -x
 
-dateISO() {
-  date -f "%s" $started -u +"%Y-%m-%dT%H:%M:%SZ"
-}
-
 started=$(date +%s)
 startedAt=$(date -u -d @$started +"%Y-%m-%dT%H:%M:%SZ")
 
-s3obj="$VERSION.tgz.aes"
+most_recent_path=$(aws s3 ls $S3_PATH --recursive | sort | tail -n 1 | awk '{print $4}') # get most recent object in bucket
+VERSION=$(echo $most_recent_path | cut -d'/' -f2-)                                       # remove bucket name from most_recent_pathj
+echo "found ${most_recent_path} as most recent object with name ${most_recent_obj_name}"
+
+s3obj=$VERSION # s3obj="$VERSION.tgz.aes"
 tarfile="restore.tgz"
 
 if [[ ! -z "${WIPE_TARGET}" && "${DATA_PATH}" != "/" ]]; then
@@ -20,8 +20,7 @@ output=$(aws s3 cp $PARAMS "${S3_PATH}/${s3obj}" "$DATA_PATH" 2>&1)
 code=$?
 if [ $code ]; then
   cd $DATA_PATH
-
-  openssl enc -aes-256-cbc -md sha512 -pbkdf2 -iter 100100 -pass "pass:${AES_PASSPHRASE}" -in $s3obj -out $tarfile -d
+  cp $s3obj $tarfile # openssl enc -aes-256-cbc -md sha512 -pbkdf2 -iter 100100 -pass "pass:${AES_PASSPHRASE}" -in $s3obj -out $tarfile -d
   ssl_code=$?
   tar xzf $tarfile
   tar_code=$?
@@ -43,7 +42,6 @@ printf "{\"restore\":{\"state\":\"restored\" } }\n"
 if [[ ! -z "$POST_RESTORE_COMMAND" && "$result" == "success" ]]; then
   restore_cmd_out=$($POST_RESTORE_COMMAND)
   printf "{\"restore\":{\"state\":\"post-command-run\", \"output\":\"%s\", \"exitCode\":\"%s\"}}\n" "$restore_cmd_out" "$?"
-
 fi
 
 finished=$(date +%s)
